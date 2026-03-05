49eae9286232df0074629674dd9338e862d3ab21
pinky <pinky.k0201@gmail.com>
2026-03-05 22:06:41 +0530

    done

--- if.py
+++ if.py
@@ -1,7 +1,7 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+numb= int(input("Enter first number: "))
+number = int(input("Enter second number: "))
 
-if num1 > num2:
-    print(num1, "is maximum")
+if numb > number:
+    print(numb, "is maximum")
 else:
-    print(num2, "is maximum")
+    print(number, "is maximum")
